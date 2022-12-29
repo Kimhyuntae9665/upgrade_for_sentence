4acf69ecbf671bff5e0775254a3543dafec8d406
rlagusxo <rlagusxo@DESKTOP-M6P2TK9>
2022-12-29 15:45:12 +0900

    ggg

---
+++ Upgrade_FOR_sentence/src/Upgrade_For_sentence.java
@@ -0,0 +1,15 @@
+
+public class Upgrade_For_sentence {
+
+	public static void main(String[] args) {
+		
+		int[] number = {1,2,3,100};
+		
+		for(int num:number) { //향상된 FOR문 
+			System.out.println(num);
+		}
+		
+		System.out.println(number.length);
+	}
+
+}
