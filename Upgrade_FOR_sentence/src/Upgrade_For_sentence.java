b5bf975abf84f2007c5fecd912ec1b9d74a14c85
rlagusxo <rlagusxo@DESKTOP-M6P2TK9>
2022-12-29 15:52:04 +0900

    again_refix

--- Upgrade_FOR_sentence/src/Upgrade_For_sentence.java
+++ Upgrade_FOR_sentence/src/Upgrade_For_sentence.java
@@ -9,10 +9,12 @@ public class Upgrade_For_sentence {
 			System.out.println(num);
 		}
 		
+		System.out.println(number); //int 는 배열 이름 넣으면 메모리 주소가 나와
+		
 		System.out.println(number.length);
 		
 		char[] str = {'h','e','l','l','o'};
-		System.out.println(str);
+		System.out.println(str); //char 배열은 이름 넣으면 문자열이 한 번에 나와
 		System.out.println(str.length);
 	}
 
